b263a329df6d5699ffe7337d82239a95211cee36
Talib <talib.superbnexus@gmail.com>
2023-04-01 21:07:34 +0600

    db connection using xml

--- src/main/java/com/alpha/jdbc/App.java
+++ src/main/java/com/alpha/jdbc/App.java
@@ -1,13 +1,23 @@
 package com.alpha.jdbc;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("My program is running");
+		// SPRING JDBC TEMPLATE
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/alpha/jdbc/config.xml");
+		JdbcTemplate dbTemplate = context.getBean("jdbcTemplate", JdbcTemplate.class);
+
+		// INSERT QUERY
+		String query = "insert into student(id, name, city) values(?, ?, ?)";
+
+		// EXECUTE QUERY
+		Integer result = dbTemplate.update(query, 102, "Zahid", "GZI");
+
+		System.out.println("Numbe of record inserted .." + result);
+
+	}
 }
